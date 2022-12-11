19e762ed4851813c71bea27690d6fb4065a38533
Loïc Poullain <loic.poullain@centraliens.net>
2022-12-11 12:32:36 +0100

    Add RSS description

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -14,7 +14,7 @@ export async function generateRssFeed() {
 
   let feed = new Feed({
     title: author.name,
-    description: 'Your blog description',
+    description: 'Loïc Poullain\'s blog',
     author,
     id: siteUrl,
     link: siteUrl,

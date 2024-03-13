6a3b38eeaeb830d12f7a88f90f9ff1daea5bff0d
Loïc Poullain <loic.poullain@protonmail.com>
2024-03-13 14:37:57 +0100

    Upgrade to Next 14

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import rehypePrism from '@mapbox/rehype-prism'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   pageExtensions: ['jsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
